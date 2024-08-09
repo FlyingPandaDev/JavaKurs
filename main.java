123d1ac1083666bd463289e21f38f95ff63c1303
Jan V <janvogele2007@icloud.com>
2024-08-09 21:24:05 +0200

    Erste Datei hinzugefuegt: Primitive und nicht primitive Datentypen.

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+  public static void main(String[] args) {
+
+    // Promitive Datentypen
+    int num = 10;
+    double preis = 10.11;
+    char note = '1';
+    boolean istJavaIntressant = true;
+
+    // Nicht primitive Datentypen:
+    String name = "FlyingPandaDev";
+    int[] nummern = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    System.out.println("Mein Name ist " + name);
+  }
+}
