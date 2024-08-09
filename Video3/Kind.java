e5f57c1b6576856a81a752fedfdf3c2ba72307fc
Jan V <janvogele2007@icloud.com>
2024-08-09 21:56:40 +0200

    Hinzufuegen des Codes des dritten Videos.

---
+++ Video3/Kind.java
@@ -0,0 +1,12 @@
+public class Kind {
+  int alter = 6;
+  String name = "EinKind";
+
+  public static void weinen() {
+    System.out.println("WuahhhhWuahhhh..");
+  }
+
+  public static void begruessen() {
+    System.out.println("Hallo!");
+  }
+}
---
+++ Video3/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    Kind kind1 = new Kind();
+    kind1.weinen();
+  }
+}
