6dddbc659e78bba9f160259e689dba4d5a61e368
Jan V <janvogele2007@icloud.com>
2024-08-09 21:35:59 +0200

    Hinzufuegen des Codes fuer das zweite Video.

---
+++ Video2/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    String begruessung = "Hallo";
+
+    int laengeDesFeldes = begruessung.length();
+
+    int zahl = 4;
+
+    int gesamt = laengeDesFeldes + zahl;
+
+    System.out.println("Die Länge des Feldes und die Zahl selbst ergeben summiert: " + gesamt);
+  }
+}
